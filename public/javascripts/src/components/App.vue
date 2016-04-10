86575948b86b816d3fc9e857ab98909df0010756
stonexer <shitianxin@gmail.com>
2016-04-10 21:44:50 +0800

    router-view bug

--- public/javascripts/src/components/App.vue
+++ public/javascripts/src/components/App.vue
@@ -22,11 +22,7 @@
 	</nav>
 
 	<div class="container">
-    <router-view
-      class="view"
-      keep-alive
-      transition
-      transition-mode="out-in">
+    <router-view>
     </router-view>
   </div>
 
